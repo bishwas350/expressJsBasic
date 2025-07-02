ee16b94c63999ce1b843b77465f4be6edf46bab6
bishwas350 <crashmindboy10@gmail.com>
2025-07-02 09:40:08 +0600

    done

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 
 
+app.use((req,res,next)=>{
+    const auth = true
+    if(auth){
+        next()
+    }else{
+        res.end('you are not allow');
+        
+    }
+        
+})
+
+
 app.get("/user",(req,res)=>{
-    res.end('hello')
+    res.end('movie is downloded')
 })
 app.listen(4000,()=>{
     console.log("server is running");
