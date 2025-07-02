3605db3680526725b3f5c2c228da5ea2e4f1ef37
bishwas350 <crashmindboy10@gmail.com>
2025-07-02 10:24:13 +0600

    node pakdge dlt

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
 
-app.use((req,res,next)=>{
-    const auth = true
-    if(auth){
-        next()
-    }else{
-        res.end('you are not allow');
+// app.use((req,res,next)=>{
+//     const auth = true
+//     if(auth){
+//         next()
+//     }else{
+//         res.end('you are not allow');
         
-    }
+//     }
         
-})
+// })
 
 
-app.get("/user",(req,res)=>{
-    res.end('movie is downloded')
-})
-app.listen(4000,()=>{
-    console.log("server is running");
+// app.get("/user",(req,res)=>{
+//     res.end('movie is downloded')
+// })
+// app.listen(4000,()=>{
+//     console.log("server is running");
     
-})
+// })
+
+
+
+class student {
+    constructor(name, year,school){
+        this.name = name
+        this.year = year
+        this.school = school
+    }
+}
+age(){
+   const date = new Date()
+    return date.getFullYear() - this.year
+}
